269cce204aa5d93887e4affce0103b0e1e61c0fa
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-11-07 12:22:01 +0100

    Modificación, del ejercicio 2 parte 2

--- prPrueba/src/application/Main.java
+++ prPrueba/src/application/Main.java
@@ -11,7 +11,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -20,72 +22,40 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			/*Button btn = new Button("Click Aqui:");
-			btn.setOnAction(new EventHandler<ActionEvent>(){
-
-				@Override
-				public void handle(ActionEvent eventz) {
-					System.out.println("Hola mundo.");
-				}
-			});
-			
-			Label lbl = new Label("Hola mundo.");
-			
-			//Para insertar cosas en el panel, lo creamos.
-			StackPane panel1 = new StackPane();
-			StackPane panel2 = new StackPane();
-			StackPane panel3 = new StackPane();
-			
-			Scene scene = new Scene(panel1,500,500);*/
-			
-			//Creamos la escena, y decimos el panel que queremos y las caracteristicas de este.
-			
-			/*Button btn1 = new Button("Boton 1");
-			Button btn2 = new Button("Boton 2");
-			Button btn3 = new Button("Boton 3");
-			Button btn4 = new Button("Boton 4");
-			
-			
-			GridPane panel = new GridPane();
-			
-			panel.setVgap(15);
-			panel.setHgap(15);
-			panel.add(btn1, 0, 0);
-			panel.add(btn2, 1, 0);
-			panel.add(btn3, 0, 1);
-			panel.add(btn4, 1, 1);*/
-			
-			/*VBox panelVertical = new VBox(15);
-			panelVertical.setPadding(new Insets(15));
-			panelVertical.getChildren().addAll(btn1,btn2,btn3);
-			BorderPane bp = new BorderPane();
-			bp.setRight(panelVertical);*/
-			/*bp.setCenter(btn1);
-			bp.setRight(btn2);
-			bp.setTop(btn3);*/
-			
-			//HBox vBoxPanel = new HBox(15);
-			//vBoxPanel.setPadding(new Insets(15));
-			//vBoxPanel.getChildren().addAll(btn1,btn2,btn3);
-			//Scene scene = new Scene(panel,400,300);
-			
-			/*primaryStage.setScene(scene);
-			primaryStage.setTitle("Introduccion a JavaFX");
-			primaryStage.getIcons().add(new Image("/application/angry.png"));
-			primaryStage.show();*/
 			
 			//Ejercicio 2. Parte 1:
 			/*StackPane stackPane = new StackPane();
 			stackPane = new StackPane();
 			stackPane.setPadding(new Insets(10));
+			//Influye el orden en el que se insertan en addAll.
 			stackPane.getChildren().addAll(new Rectangle(500, 500, Color.BLUEVIOLET),new Rectangle(400, 400, Color.CORAL), new Rectangle(300, 300, Color.GOLD));
 	       
 	        Scene scene = new Scene(stackPane);
 	        primaryStage.setScene(scene);
 	        primaryStage.show();*/
 			
-			//Ej 2
-			Button boton1 = new Button("1");
+			//Ejercicio 2. Parte 2:
+			BorderPane bp = new BorderPane();
+			
+			Rectangle rCentro = new Rectangle(300, 300, Color.AQUAMARINE);
+			Rectangle rArriba = new Rectangle(400, 50, Color.CORAL);
+			Rectangle rAbajo = new Rectangle(400, 50, Color.CORAL);
+			Rectangle rDerecha = new Rectangle(50, 300, Color.BLUEVIOLET);
+			Rectangle rIzquierda = new Rectangle(50, 300, Color.BLUEVIOLET);
+			
+			bp.setCenter(rCentro);
+			bp.setTop(rArriba);
+			bp.setBottom(rAbajo);
+			bp.setRight(rDerecha);
+			bp.setLeft(rIzquierda);
+			
+			Scene scene = new Scene(bp);
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+			
+			
+			//Ejercicio 3. Teclado numérico:
+			/*Button boton1 = new Button("1");
 			Button boton2 = new Button("2");
 			Button boton3 = new Button("3");
 			Button boton4 = new Button("4");
@@ -117,7 +87,7 @@ public class Main extends Application {
 
 			Scene s = new Scene(panel,300,300);
 		    primaryStage.setScene(s);
-		    primaryStage.show();
+		    primaryStage.show();*/
 			
 			
 			
