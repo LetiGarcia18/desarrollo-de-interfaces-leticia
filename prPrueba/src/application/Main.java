56e685c060c682bfea62059659b7dd0290787544
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-11-07 09:20:07 +0100

    Ejercicio del teclado numérico subido

--- prPrueba/src/application/Main.java
+++ prPrueba/src/application/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -74,46 +75,49 @@ public class Main extends Application {
 			primaryStage.show();*/
 			
 			//Ejercicio 2. Parte 1:
-			StackPane stackPane = new StackPane();
+			/*StackPane stackPane = new StackPane();
 			stackPane = new StackPane();
 			stackPane.setPadding(new Insets(10));
 			stackPane.getChildren().addAll(new Rectangle(500, 500, Color.BLUEVIOLET),new Rectangle(400, 400, Color.CORAL), new Rectangle(300, 300, Color.GOLD));
 	       
 	        Scene scene = new Scene(stackPane);
 	        primaryStage.setScene(scene);
-	        primaryStage.show();
+	        primaryStage.show();*/
 			
 			//Ej 2
-			/*Button btn1 = new Button("1");
-			Button btn2 = new Button("2");
-			Button btn3 = new Button("3");
-			Button btn4 = new Button("4");
-			Button btn5 = new Button("5");
-			Button btn6 = new Button("6");
-			Button btn7 = new Button("7");
-			Button btn8 = new Button("8");
-			Button btn9 = new Button("9");
-			Button btn0 = new Button("0");
-			Button btnLlamar = new Button("Llamar");
-			Button btnColgar = new Button("Colgar");
+			Button boton1 = new Button("1");
+			Button boton2 = new Button("2");
+			Button boton3 = new Button("3");
+			Button boton4 = new Button("4");
+			Button boton5 = new Button("5");
+			Button boton6 = new Button("6");
+			Button boton7 = new Button("7");
+			Button boton8 = new Button("8");
+			Button boton9 = new Button("9");
+			Button boton0 = new Button("0");
+			Button botonLlamar = new Button("Llamar");
+			Button botonColgar = new Button("Colgar");
 			
 			GridPane panel = new GridPane();
 			
 			panel.setVgap(0);
 			panel.setHgap(0);
-			panel.add(btn1, 0, 2);
-			panel.add(btn2, 1, 2);
-			panel.add(btn3, 2, 2);
-			panel.add(btn4, 0, 1);
-			panel.add(btn5, 1, 1);
-			panel.add(btn6, 2, 1);
-			panel.add(btn7, 0, 0);
-			panel.add(btn8, 1, 0);
-			panel.add(btn9, 2, 0);
-			panel.add(btn0, 1, 3);
-			panel.add(btnLlamar, 3, 1);
-			panel.add(btnColgar, 3, 2);*/
+			panel.add(boton1, 0, 2);
+			panel.add(boton2, 1, 2);
+			panel.add(boton3, 2, 2);
+			panel.add(boton4, 0, 1);
+			panel.add(boton5, 1, 1);
+			panel.add(boton6, 2, 1);
+			panel.add(boton7, 0, 0);
+			panel.add(boton8, 1, 0);
+			panel.add(boton9, 2, 0);
+			panel.add(boton0, 1, 3);
+			panel.add(botonLlamar, 3, 1);
+			panel.add(botonColgar, 3, 2);
 
+			Scene s = new Scene(panel,300,300);
+		    primaryStage.setScene(s);
+		    primaryStage.show();
 			
 			
 			
