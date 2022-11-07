6283a8fa5c41ef2c66a2889b12f7e14777071852
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-11-07 13:18:00 +0100

    Se añade funcionalidad a los botones de números

--- ejercicio3.extra/src/application/Main.java
+++ ejercicio3.extra/src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -29,25 +32,107 @@ public class Main extends Application {
 			Button botonColgar = new Button("Colgar");
 			
 			//Panel principal
-			StackPane panel = new StackPane();
+			BorderPane panel = new BorderPane();
+			Label label_numero = new Label();
+			panel.setBottom(label_numero);
+			
 			
 			//Panel secundario
 			GridPane grid = new GridPane();
-			panel.setAlignment(grid, Pos.CENTER); //Esto es para alinearlo
-			panel.getChildren().addAll(grid);
+			panel.setCenter(grid); //Esto es para alinearlo
 			
 			grid.setVgap(0);
 			grid.setHgap(0);
 			grid.add(boton1, 0, 2); //Primero va la columna (0), y luego la fila (2)
+			boton1.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "1";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton2, 1, 2);
+			boton2.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "2";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton3, 2, 2);
+			boton3.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "3";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton4, 0, 1);
+			boton4.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "4";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton5, 1, 1);
+			boton5.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "5";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton6, 2, 1);
+			boton6.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "6";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton7, 0, 0);
+			boton7.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "7";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton8, 1, 0);
+			boton8.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "8";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton9, 2, 0);
+			boton9.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "9";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(boton0, 1, 3);
+			boton0.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent event) {
+					String numero = label_numero.getText() + "0";
+					label_numero.setText(numero);
+				}
+				
+			});
 			grid.add(botonLlamar, 3, 1);
 			grid.add(botonColgar, 3, 2);
 
