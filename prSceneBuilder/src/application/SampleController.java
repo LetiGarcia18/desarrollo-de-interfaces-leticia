32d3933151c5cf1a8b4c2b8d3dad84e076f02bd5
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-11-14 12:51:36 +0100

    Primer contacto con el SceneBuilder

--- prSceneBuilder/src/application/SampleController.java
+++ prSceneBuilder/src/application/SampleController.java
@@ -1,5 +1,24 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SampleController {
 	
+	@FXML
+	private TextField textoMensaje;
+	
+	@FXML
+	private Button botonMensaje;
+	
+	@FXML
+	private Label labelMensaje;
+	
+	@FXML
+	public void mostrarMensaje(ActionEvent event) {
+		labelMensaje.setText(textoMensaje.getText()); //De esta manera cogemos el texto que se ha introducido en el TextField
+	}
 }
